43ed8bd2af4b0cf2e8c5fdc5bb81de6251e69db7
Arthur <arthur.bgarnier@outlook.fr>
2021-10-12 15:44:56 -0400

    button state

--- EOSFrontEnd/screens/AddPostScreen.tsx
+++ EOSFrontEnd/screens/AddPostScreen.tsx
@@ -6,23 +6,34 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import ActionButton from '../components/ActionButton';
 import ActionButtonSecondary from '../components/ActionButtonSecondary';
+import { useState } from 'react';
 
 export default function AddPostScreen() {
-  return (
+  const [step, setStep] = useState(1)
+  const [selected, setSelected] = useState('');
+  if (step == 1)
+    return (
     <View style={styles.container}>
       <Text style={styles.title}>AddPost</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={styles.innerContainer}>
         <Text style={styles.subTitle}>You are intersted in...</Text>
         <View style={styles.buttonContainer}>
-          <ActionButtonSecondary  title="Items" onPress={() => console.log('clicked')}></ActionButtonSecondary>
-            <ActionButtonSecondary styleContainer={{marginTop: 10,}} title="Services" onPress={() => console.log('clicked')}></ActionButtonSecondary>
-
+          <ActionButtonSecondary  title="Items" styleContainer={selected == 'Items' ? {backgroundColor: '#04B388'} : {backgroundColor: 'white'}} styleText={selected == 'Items' ? {color: 'white'} : {color: '#04B388'}} onPress={() => {setSelected('Items')}}></ActionButtonSecondary>
+            <ActionButtonSecondary styleContainer={[{marginTop: 10}, selected == 'Services' ? {backgroundColor: '#04B388'} : {backgroundColor: 'white'}]} styleText={selected == 'Services' ? {color: 'white'} : {color: '#04B388'}} title="Services" onPress={() => {setSelected('Services')}}></ActionButtonSecondary>
+            <ActionButton title="Next" onPress={() => {setStep(2); console.log(step)}}></ActionButton>
         </View>
         
       </View>
     </View>
   );
+  else 
+      return (
+      <View style={styles.container}>
+        <Text style={styles.title}>AddPost</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </View>
+      )
 }
 
 const styles = StyleSheet.create({
