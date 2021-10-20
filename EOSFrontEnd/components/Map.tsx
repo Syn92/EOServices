c77e825a0eb3e514153ab51d96390124bce8a621
Guilhem <guilhem.dubois@polymtl.ca>
2021-10-20 14:35:00 -0400

    peut cliquer sur cadastre et retourne addresse

--- EOSFrontEnd/components/Map.tsx
+++ EOSFrontEnd/components/Map.tsx
@@ -3,6 +3,8 @@ import { View, StyleSheet, Platform } from 'react-native';
 import MapView, { Geojson, LatLng, MapEvent, Marker, Region, UrlTile } from 'react-native-maps';
 import Geocoder from 'react-native-geocoding';
 import axios from 'axios';
+import { isPointInPolygon } from 'geolib';
+import { GeolibInputCoordinates } from 'geolib/es/types';
 
 interface IMarker {
     key: string;
@@ -17,6 +19,13 @@ interface IProps {
     onPressed?: Function;
 }
 
+interface IFeatureProperties {
+    ID_UEV: string,
+    CIVIQUE_DEBUT: string,
+    NOM_RUE: string,
+    MUNICIPALITE: string,
+}
+
 Geocoder.init("AIzaSyCcPFzHoC-XT8h-3MZt8CfIz5J-w9BeMHA");
 
 const initialRegion: Region = {
@@ -29,7 +38,14 @@ const initialRegion: Region = {
 export default function Map(props: IProps) {
     const [markers, setMarkers] = useState<IMarker[]>([]);
 
-    const [geoJson, setGeoJson] = useState<GeoJSON.FeatureCollection>(
+    const [geoJson, setGeoJson] = useState<GeoJSON.FeatureCollection<GeoJSON.Geometry, IFeatureProperties>>(
+        {
+            type: "FeatureCollection",
+            features: []
+        }
+    );
+
+    const [selectedGeoJson, setSelectedGeoJson] = useState<GeoJSON.FeatureCollection>(
         {
             type: "FeatureCollection",
             features: []
@@ -43,20 +59,31 @@ export default function Map(props: IProps) {
     // if pressable, react to the onPress event by adding a marker
     // and calling onPressed with the corresponding address
     function mapPressed(event: MapEvent) {
-        if(!props.pressable) return;
-
-        const marker: IMarker = {
-            key: 'pressedMarker',
-            coordinate: event.nativeEvent.coordinate,
-        }
-        addMarker(marker);
+        // if(!props.pressable) return;
 
         if(props.onPressed) {
-        Geocoder.from(marker.coordinate).then(json => {
-            const address = json.results[0].formatted_address;
-            if(props.onPressed) props.onPressed(address);
-        }).catch(error => console.warn(error));
-        //todo: replace with checking on the cadastre foreach
+            // Geocoder.from(marker.coordinate).then(json => {
+            //     const address = json.results[0].formatted_address;
+            //     if(props.onPressed) props.onPressed(address);
+            // }).catch(error => console.warn(error));
+            const feature = geoJson.features.find(x => {
+                const polygon = (x.geometry as GeoJSON.Polygon)?.coordinates[0] as GeolibInputCoordinates[];
+                if(!polygon) return false;
+                return isPointInPolygon(event.nativeEvent.coordinate, polygon);
+            });
+            if(feature) {
+                const marker: IMarker = {
+                    key: 'pressedMarker',
+                    coordinate: event.nativeEvent.coordinate,
+                }
+                setSelectedMarker(marker);
+                setSelectedGeoJson({type: selectedGeoJson.type, features: [feature]});
+                if(props.onPressed) props.onPressed(feature.properties.CIVIQUE_DEBUT + " " + feature.properties.NOM_RUE)
+                console.log('vertices: ', (feature.geometry as GeoJSON.Polygon).coordinates[0].length);
+            } else {
+                setSelectedMarker(null);
+                setSelectedGeoJson({type: selectedGeoJson.type, features: []});
+            }
         }
     }
 
@@ -71,15 +98,17 @@ export default function Map(props: IProps) {
             latitude: region.latitude,
             longitude: region.longitude
         }
-        const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
-        }
-        axios.get('http://10.200.12.162:4000/cadastre', { params, headers},)
+        // const headers = {
+        //     'Content-Type': 'application/x-www-form-urlencoded',
+        //     'Accept': 'application/json'
+        // }
+        axios.get('http://10.200.12.162:4000/cadastre', { params },)
             .then(function (response) {
                 // handle success
-                const features = response.data as GeoJSON.Feature[];
+                const features = response.data as GeoJSON.Feature<GeoJSON.Geometry, IFeatureProperties>[];
                 setGeoJson({type: geoJson.type, features: features});
+                // const maxVertices = Math.max.apply(Math, (features.map(x => (x.geometry as GeoJSON.Polygon).coordinates.length)))
+                // console.log(maxVertices);
             }).catch(function (error) {
                 // handle error
                 console.log(error);
@@ -90,12 +119,18 @@ export default function Map(props: IProps) {
     }
 
     // add a single marker. do not use if adding more than one at once
-    function addMarker(marker: IMarker) {
-        const existingIndex = markers.findIndex(x => x.key == marker.key);
-        if(existingIndex != -1) {
-            markers[existingIndex] = marker;
-        } else {
-            markers.push(marker);
+    function setSelectedMarker(marker: IMarker | null) {
+        const markerKey = "selectedMarker";
+        const existingIndex = markers.findIndex(x => x.key == markerKey);
+        if(marker) {
+            marker.key = markerKey;
+            if(existingIndex > -1) {
+                markers[existingIndex] = marker;
+            } else {
+                markers.push(marker);
+            }
+        } else if(existingIndex > -1) {
+            markers.splice(existingIndex, 1);
         }
         setMarkers(markers);
     }
@@ -108,7 +143,8 @@ export default function Map(props: IProps) {
         <View style={styles.container}>
             <MapView style={styles.map} initialRegion={initialRegion} onPress={mapPressed}
                 mapType={Platform.OS == "android" ? "none" : "standard"} onRegionChangeComplete={regionChanged}>
-                <Geojson geojson={geoJson} strokeColor="blue" fillColor="rgba(0, 255, 255, 0.2)" strokeWidth={2} zIndex={2}></Geojson>
+                <Geojson geojson={selectedGeoJson} strokeColor="black" fillColor="green" strokeWidth={2} zIndex={3}></Geojson>
+                <Geojson geojson={geoJson} strokeColor="blue" fillColor="rgba(0, 255, 255, 0.2)" strokeWidth={1} zIndex={2}></Geojson>
                 <UrlTile urlTemplate='https://api.maptiler.com/maps/streets/{z}/{x}/{y}@2x.png?key=eif7poHbo0Lyr1ArRDWL'
                     maximumZ={19} zIndex={1}
                 />
@@ -124,7 +160,7 @@ export default function Map(props: IProps) {
 function renderMarkers(markers: IMarker[]): JSX.Element[] {
     return markers.map((marker) => {
         return (
-            <Marker key={marker.key} coordinate={marker.coordinate} tracksViewChanges={false}/>
+            <Marker key={marker.key} coordinate={marker.coordinate} tracksViewChanges={false} zIndex={5}/>
         )
     });
 }
