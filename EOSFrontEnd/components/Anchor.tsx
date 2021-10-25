824ccb348a752b207a08fea78b92c5b673d17343
NicoHirab <43252820+NicoHirab@users.noreply.github.com>
2021-10-25 18:01:13 -0400

    added anchorlinkreacttrabsport

---
+++ EOSFrontEnd/components/Anchor.tsx
@@ -0,0 +1,51 @@
+import AnchorLink, { LinkSession } from 'anchor-link';
+import AnchorLinkReactTransport from '../AnchorLinkReactTransport/AnchorLinkReactTransport'
+
+import { EOSIO_CHAIN_ID, EOSIO_RPC } from '../constants/AnchorConfig';
+
+(global as any).fetch = fetch;
+
+export const link = new AnchorLink({
+  transport: new AnchorLinkReactTransport(),
+  chains: [
+    {
+      chainId: EOSIO_CHAIN_ID,
+      nodeUrl: EOSIO_RPC,
+    },
+  ],
+});
+
+export async function getAccount() {
+  console.log('anchor::getAccount');
+  return sessionToAccount(await login());
+}
+
+export async function disconnect() {
+  console.log('anchor::disconnect');
+  try {
+    await link.clearSessions('app.io');
+  } catch (err) {
+    console.log('anchor::disconnect', { err });
+  }
+}
+
+function sessionToAccount(session: LinkSession | null) {
+  console.log('anchor::sessionToAccount');
+  if (!session) return {};
+  const { auth, publicKey } = session;
+  const { actor, permission } = auth;
+  return {
+    actor: actor.toString(),
+    permission: permission.toString(),
+    publicKey: publicKey.toString(),
+    authorization: auth.toString(),
+  };
+}
+
+export async function login() {
+    // console.log('anchor::login');
+    // const sessions = await link.listSessions('pomelo.io');
+    // if (sessions.length) return await link.restoreSession('pomelo.io');
+    // else return (await link.login('pomelo.io')).session;
+    return (await link.login('eos-marketplace')).session
+  }
